1cad83f171ab344eea5eb2f9855a04b74d09adc4
AzharChowdhurys <azharchowdhury110@gmail.com>
2025-11-11 22:36:26 +0600

    Updating index.tsx with new content

---
+++ index.tsx
@@ -0,0 +1,19 @@
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Writings from "@/components/Writings";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Writings />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
